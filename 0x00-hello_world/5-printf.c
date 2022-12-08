67bc86880c38b284177caeed9e518ac52c6d23a6
Thegr8test <wilsonjulius36@gmail.com>
2022-12-08 09:08:26 -0800

    usual printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - pints a line
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (o);
+}
