8407cc2a6d57befd624a11cb0559ab6a4c1120d4
Thegr8test <wilsonjulius36@gmail.com>
2022-12-08 08:50:34 -0800

    puts instead of printf, interesting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/*
+ * main - print a line of words using the put function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retorn (0);
+}
