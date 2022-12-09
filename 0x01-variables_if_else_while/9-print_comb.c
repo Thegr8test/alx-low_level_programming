e262a8a8990b5d867ed89984ff2255fd437dc081
Thegr8test <wilsonjulius36@gmail.com>
2022-12-09 13:32:48 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar('\n');
-		return (0);
+		putchar((n % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
+
+	return (0);
+}
