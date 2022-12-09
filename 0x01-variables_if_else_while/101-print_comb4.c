3ed3ee14f94e311821f0e60e120682dc89acf59a
Thegr8test <wilsonjulius36@gmail.com>
2022-12-09 13:58:42 -0800

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combos of three different digits up and down
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int dig1, dig2, dig3;
+
+	for (dig1 = 0; dig2 < 8; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 9; dig2++)
+		{
+			for (dig3 = dig 2 + 1; dig3 < 10; dig3++)
+			{
+				putchar((dig1 % 10) + '0');
+				putchar((dig2 % 10) + '0');
+				putchar((dig3 % 10) + '0');
+
+				if (dig1 == 7 && dig2 == 8 && dig3++ 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
