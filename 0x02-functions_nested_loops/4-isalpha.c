fd00a4112f61dbd9f42e81262831cce076bb92da
Thegr8test <wilsonjulius36@gmail.com>
2022-12-14 03:06:34 -0800

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
   *_isalpha - checks whether char is alphabet
   *Return: 1 if char is a letter, 0 if not
   */
-int _isalphabet(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
