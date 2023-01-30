41de691b4f5711bb55aca088cb78c6d84d1dcd39
Thegr8test <wilsonjulius36@gmail.com>
2023-01-30 08:24:40 -0800

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * funstory - executes the line of befor main funct
+ */
+void funstory(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void funstory(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
