619f9ccfd75e9edb17c7d2abc83a7aec78c5b69a
Thegr8test <wilsonjulius36@gmail.com>
2023-02-08 10:11:44 -0800

    correction on betty complaint

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - funct that reads a text file and prints it to POSIX stdout.
+ * read_textfile - funct that reads a text
+ *                 file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
  *           function should read and print.
